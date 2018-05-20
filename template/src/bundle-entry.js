28b44adc81828024f34896592db64fae4dd56aac
phphe <phphe@outlook.com>
2018-05-20 09:42:29 +0800

    bili.js integrated

---
+++ template/src/bundle-entry.js
@@ -0,0 +1,7 @@
+// example
+// single component
+// export * from './components/???.vue'
+//
+// multiple components
+// export {default as Component1} from './components/Component1.vue'
+// export {default as Component2} from './components/Component2.vue'
